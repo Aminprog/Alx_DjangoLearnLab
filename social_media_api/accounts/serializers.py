1696b1f81e601f0501427e6fd9de17de223a2294
Aminprog <fissaa.amin@gmail.com>
2025-04-04 16:14:12 +0000

    second commit

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -14,10 +14,14 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields = ['username', 'password', 'email']
         extra_kwargs = {'password': {'write_only': True}}
 
+    from django.contrib.auth import get_user_model
+    User = get_user_model()
+
     def create(self, validated_data):
-        user = CustomUser.objects.create_user(**validated_data)
+        user = User.objects.create_user(**validated_data)
         Token.objects.create(user=user)
         return user
+
 
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
